deb5706e8f5c9998041e0acd8a0856d290f0bac6
Vitaliy Oleinik <42870696+VitaliyOleinik@users.noreply.github.com>
2021-02-16 15:30:31 +0600

    leetcode task

---
+++ Week-3/LeetCodeTasks/src/Task7.scala
@@ -0,0 +1,12 @@
+object seven {
+  def kWeakestRows(mat: Array[Array[Int]], k: Int): Array[Int] = {
+
+    var soldiers = Array.ofDim[Int](mat.size)
+
+    for (column <- 0 until mat.size; row <- 0 until mat.size) {
+      if (mat(column)(row) == 1) soldiers(column) += 1
+    }
+
+    soldiers.zipWithIndex.sortBy(c => c._1).take(k).unzip._2
+  }
+}
