005aa24e05b6c1d2ae28e660a8c2e9e9daa19da7
VitaliyOleinik <oleynik624@gmail.com>
2021-04-26 14:40:46 +0600

    class work with Docker

---
+++ Week-10/Week10/src/main/scala/InMemoryTodoRepository.scala
@@ -0,0 +1,27 @@
+import java.util.UUID
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class InMemoryTodoRepository(initialTodos:Seq[Todo] = Seq.empty)(implicit ec:ExecutionContext) extends TodoRepository {
+
+  private var todos: Vector[Todo] = initialTodos.toVector
+
+  override def all(): Future[Seq[Todo]] = Future.successful(todos)
+
+  override def done(): Future[Seq[Todo]] = Future.successful(todos.filter(_.done))
+
+  override def pending(): Future[Seq[Todo]] = Future.successful(todos.filterNot(_.done))
+
+  override def create(createTodo: CreateTodo): Future[Todo] =
+    Future.successful {
+      val todo = Todo(
+        id = UUID.randomUUID().toString,
+        title = createTodo.title,
+        description = createTodo.description,
+        done = false
+      )
+      todos = todos :+ todo
+      todo
+    }
+
+}
